af655582d45fbec6577e6ad6c7ddac71bbd9c0eb
Matteo Rebeschini <matteo@elastic.co>
2019-08-23 14:53:20 -0600

    Update instance-setup.sh

--- instance-setup.sh
+++ instance-setup.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+ELASTIC_DIR=/home/ubuntu/elastic
+mkdir $ELASTIC_DIR
+wget https://raw.githubusercontent.com/mrebeschini/zeek-elastic-workshop/master/zeek-ingest-pipeline.yml -P $ELASTIC_DIR
+wget https://raw.githubusercontent.com/mrebeschini/zeek-elastic-workshop/master/filebeat.yml -P $ELASTIC_DIR
