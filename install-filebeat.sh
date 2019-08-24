d472ded1766c887e9b7ebef17ccf3014ac18d2a9
Matteo Rebeschini <matteo@elastic.co>
2019-08-24 09:17:59 -0600

    Update install-filebeat.sh

--- install-filebeat.sh
+++ install-filebeat.sh
@@ -7,4 +7,3 @@ sudo cp /home/ubuntu/elastic/filebeat.yml /etc/filebeat/
 sudo filebeat modules enable zeek                                                                                                                                                              
 sudo cp -f /home/ubuntu/elastic/zeek.yml /etc/filebeat/modules.d/ 
 sudo update-rc.d filebeat defaults 95 10
-sudo filebeat setup
