b25d6945839d68c1f48fa43a821ef4256a89de04
Matteo Rebeschini <matteo@elastic.co>
2019-08-23 18:09:46 -0600

    Update instance-setup.sh

--- instance-setup.sh
+++ instance-setup.sh
@@ -8,7 +8,6 @@ ZEEK_DIR=/home/ubuntu/zeek
 mkdir -p $ZEEK_DIR/logs
 wget https://www.zeek.org/downloads/bro-2.6.3.tar.gz -P $ZEEK_DIR
 wget https://github.com/mrebeschini/zeek-elastic-workshop/raw/master/zeek-logs.tar.gz -P /tmp
-tar -xfvz /tmp/zeek-logs.tar.gz -C $ZEEK_DIR/logs
-#rm -f /tmp/zeek-logs.tar.gz
+tar xfvz /tmp/zeek-logs.tar.gz -C $ZEEK_DIR/logs
 for i in $ZEEK_DIR/logs/*.log.gz; do gzip -d $i; done
 chown -R ubuntu:ubuntu $ZEEK_DIR
